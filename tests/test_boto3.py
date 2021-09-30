4e1927a2847fb405ddd7ca2a8f01cb032cc7386d
Andrew Kane <andrew@ankane.org>
2021-09-30 14:26:12 -0700

    Fixed CI

--- tests/test_boto3.py
+++ tests/test_boto3.py
@@ -7,12 +7,12 @@ from botocore.exceptions import ConnectTimeoutError, ReadTimeoutError
 class TestBoto3(TestTimeouts):
     def test_connect(self):
         config = Config(connect_timeout=1, retries={'max_attempts': 0})
-        client = boto3.client('s3', config=config, endpoint_url=self.connect_url())
+        client = boto3.client('s3', config=config, endpoint_url=self.connect_url(), aws_access_key_id='akid', aws_secret_access_key='secret')
         with self.raises(ConnectTimeoutError):
             client.list_buckets()
 
     def test_read(self):
         config = Config(read_timeout=1, retries={'max_attempts': 0})
-        client = boto3.client('s3', config=config, endpoint_url=self.read_url())
+        client = boto3.client('s3', config=config, endpoint_url=self.read_url(), aws_access_key_id='akid', aws_secret_access_key='secret')
         with self.raises(ReadTimeoutError):
             client.list_buckets()
