4ff09a1aa46f7a0c34a22521c82dd7f674c904f2
Andrew Kane <andrew@ankane.org>
2021-10-03 11:09:39 -0700

    Removed extra subprocess test [skip ci]

--- tests/test_subprocess.py
+++ tests/test_subprocess.py
@@ -3,10 +3,6 @@ from subprocess import run, TimeoutExpired
 
 
 class TestSubprocess(TestTimeouts):
-    def test_connect(self):
-        with self.raises(TimeoutExpired):
-            run('sleep 10', shell=True, timeout=1)
-
-    def test_read(self):
+    def test_timeout(self):
         with self.raises(TimeoutExpired):
             run('sleep 10', shell=True, timeout=1)
